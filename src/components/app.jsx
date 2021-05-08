337600341d82c44bab0fe486f841332bccdd0763
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-05-08 15:16:33 +0800

    created the app

---
+++ src/components/app.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx'
+
+const GIPHY_API_KEY = 'YGiomHSm53r8MXViEq9IqjsxtUtg2Olj';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId : "em4rewukzmsO7HfJc7"
+    }
+    this.search = this.search.bind(this);
+    this.selectGif = this.selectGif.bind(this);
+  }
+
+  search(query) {
+    const giphEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${query}&limit=10`
+    fetch(giphEndpoint).then(response => response.json()).then((data) => {
+      const gifs = data.data.map(giph => giph.id)
+      this.setState({
+        gifs: gifs
+      })
+    })
+  }
+
+  selectGif(id) {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
